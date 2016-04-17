43c3221e67226a035d935fb989cdbbefe664561a
AwesomeShayne <shayne.hemminger@louisville.edu>
2016-04-16 21:44:22 -0400

    Prepared the listener code for the arduinos.

---
+++ hearYourHomeListener.cpp
@@ -0,0 +1,26 @@
+/*
+Purpose:    This program is designed to listen for preset signals, for example: Ovens, 
+            Microwaves, Washers, Dryers, Doorbells, and produce a LISNR Signal to notify 
+            the homeowner
+Developer:  Shayne Hemminger, Tyler Ault, Jacob Berdichevsky
+*/
+
+
+// This next line is important if you are running this code on a photon rather than an 
+// arduino.
+// This defeats the purpose of using this project. But can be used for testing purposes.
+// #include "application.h" 
+
+
+// Initialize Variables
+
+
+// Set pin purposes
+void setup() {
+    
+}
+
+// This code executes constantly while the device is on.
+void loop() {
+    
+}
